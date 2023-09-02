cd9e2b24cba0399480d77e00c5c6017c543076ed
EduDsantos <eduardo.dev66@gmail.com>
2023-09-02 20:48:17 -0300

    cronometro finalizado

---
+++ timer/script.js
@@ -0,0 +1,53 @@
+let zero = document.getElementById('num');
+
+let seg= 0;
+let min= 0;
+let hour= 0;
+let inter;
+
+
+
+function play(){
+    inter = setInterval(timer,1);
+
+}
+
+function pause(){
+     clearInterval(inter);
+    
+}
+
+function reset(){
+    clearInterval(inter);
+    seg=0;
+    min=0;
+    hour=0;
+    zero.innerHTML = "00:00:00";
+    
+}
+
+function timer(){
+    seg++;
+    if(seg == 60){
+       seg = 0;
+       min++;
+
+       if(min == 60){
+        min = 0;
+        hour++;
+    }
+    
+    }
+    zero.innerHTML = twodec(hour) + ":" + twodec(min) + ":" + twodec(seg);
+   
+
+}
+
+function twodec(deci){
+    if(deci < 10){
+        return ('0' + deci);
+    }else{
+        return(deci)
+    }
+   
+}
